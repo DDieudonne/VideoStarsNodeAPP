4b318613251ab8c9d534a4675b9ded4bdb4b448a
Dieudonné <lando-dieudonne@hotmail.fr>
2018-02-16 03:17:06 +0100

    Update config.js

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ const mysql = require('promise-mysql');
 const connection = mysql.createConnection({
     host: 'localhost', // our serveur
     user: 'root', // its username of database or server
-    password: "Dtoxpopnbrother95170", // its database password
+    password: "iuhujzjcis862cs6", // its database password
     database: 'videostars'    //its name of database on mysql
 });
 
